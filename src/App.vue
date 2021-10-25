fb83da46adc34ab58fd23b5223ad7a8755b501ec
Nadav Gur-Arieh <nadavga@gmail.com>
2021-10-25 15:34:15 +0300

    up to lesson 35

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-   <add-blog></add-blog>
+    <add-blog></add-blog>
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
-import addBlog from './components/addBlog.vue'
+import addBlog from "./components/addBlog.vue";
+import showBlogs from "./components/showBlogs.vue";
+
 export default {
-  components:{
-    'add-blog':addBlog
+  components: {
+    "add-blog": addBlog,
+    "show-blogs": showBlogs,
   },
-  data () {
-    return {
-      
-    }
-  }
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-body{
-  margin:0;
-  font-family: 'Nunito SemiBold';
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
 
+//Custom directives
+Vue.directive('rainbow',{
+  bind(el, binding, vnode){
+    el.style.color = "#"+[...Array(6)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+  }
+});
+Vue.directive('theme',{
+  bind(el, binding, vnode){
+    if(binding.value=='wide'){
+      el.style.maxWidth="1200px"
+    }else if (binding.value=='narrow') {
+      el.style.maxWidth="560px"
+    }
+    if(binding.arg=='column'){
+      el.style.background='#ddd';
+      el.style.padding='20px';
+    }
+  }
+});
+
+//Filters
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
+});
+Vue.filter('snippet', function(value){
+  return value.slice(0,100)+"...";
+});
 new Vue({
   el: '#app',
   render: h => h(App)
